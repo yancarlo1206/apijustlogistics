1afcb7e5034d218f002b5c5758171296346530c2
dafemoncadausb <123998746+dafemoncadausb@users.noreply.github.com>
2025-12-02 21:03:44 -0500

    servicos clienteproceso y lieasproceso

--- src/main/java/com/justlogistics/repository/ProcesoRepository.java
+++ src/main/java/com/justlogistics/repository/ProcesoRepository.java
@@ -1,9 +1,13 @@
 package com.justlogistics.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.justlogistics.entity.Proceso;
 
+
 public interface ProcesoRepository extends JpaRepository<Proceso, Integer> {
+	
+	List<Proceso> findByClienteId(Integer clienteId);
 
 }
--- src/main/java/com/justlogistics/service/ClienteService.java
+++ src/main/java/com/justlogistics/service/ClienteService.java
@@ -7,49 +7,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.justlogistics.entity.Cliente;
+import com.justlogistics.entity.Usuario;
 import com.justlogistics.repository.ClienteRepository;
+import com.justlogistics.repository.UsuarioRepository;
 
 @Service
 public class ClienteService {
-	
-	 @Autowired
-	 private ClienteRepository clienteRepository;
-	
-		public Cliente guardar(Cliente request) {
 
-			request.setFechacreacion(LocalDateTime.now());
-			request.setFechaactualizacion(LocalDateTime.now());
-			return clienteRepository.save(request);
-		}
-	    
-    public List<Cliente> listar(){
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	public Cliente guardar(Cliente request) {
+
+		request.setFechacreacion(LocalDateTime.now());
+		request.setFechaactualizacion(LocalDateTime.now());
+		return clienteRepository.save(request);
+	}
+
+	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
-	
+
 	public Cliente get(Integer id) {
-		return clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
+		return clienteRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
 	}
-	
-	public Cliente actualizar(Integer id,Cliente request) {
-	   Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));    
-      
-	   cliente.setNit(request.getNit());
-	   cliente.setRazonsocial(request.getRazonsocial());
-	   cliente.setNombrecontacto(request.getNombrecontacto());
-	   cliente.setApellidocontacto(request.getApellidocontacto());
-	   cliente.setCorreo(request.getCorreo());
-	   cliente.setTelefono(request.getTelefono());
-	   cliente.setFechaactualizacion(LocalDateTime.now());
-	   return clienteRepository.save(cliente);
+
+	public Cliente actualizar(Integer id, Cliente request) {
+		Cliente cliente = clienteRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
+
+		cliente.setNit(request.getNit());
+		cliente.setRazonsocial(request.getRazonsocial());
+		cliente.setNombrecontacto(request.getNombrecontacto());
+		cliente.setApellidocontacto(request.getApellidocontacto());
+		cliente.setCorreo(request.getCorreo());
+		cliente.setTelefono(request.getTelefono());
+		cliente.setFechaactualizacion(LocalDateTime.now());
+		return clienteRepository.save(cliente);
 	}
-	
+
 	public boolean eliminar(Integer id) {
-        if (clienteRepository.existsById(id)) {
-        	clienteRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
+		if (clienteRepository.existsById(id)) {
+			clienteRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	public String obtenerNombreCliente(Integer clienteId) {
+
+		Cliente cliente = clienteRepository.findById(clienteId)
+				.orElseThrow(() -> new RuntimeException("Cliente no encontrado con usuario: " + clienteId));
+
+		return cliente.getNombrecontacto() + " " + cliente.getApellidocontacto();
+	}
+
+	public Integer obtenerClienteIdPorUsername(String username) {
+
+		Cliente cliente = clienteRepository.findByNit(username)
+				.orElseThrow(() -> new RuntimeException("Cliente no encontrado: " + username));
+
+		return cliente.getId();
+	}
 
 }
-
